9324d48089c13fb2d0d7d233d22f482559a6b2ad
Cindy Leschaud <cindy.leschaud@gmail.com>
2016-06-20 11:25:28 +0200

    add ititle du mois to send partial

--- resources/views/backend/newsletter/send/groupe.blade.php
+++ resources/views/backend/newsletter/send/groupe.blade.php
@@ -24,7 +24,8 @@
             <tr>
                 <td valign="top" width="375" class="resetMarge contentForm">
                     <div>
-                        <h3 style="text-align: left;font-family: sans-serif;">{{ $arret->reference }} du {{ $arret->pub_date->formatLocalized('%d %B %Y') }}</h3>
+                        <?php $title = ($arret->dumois ? 'Arrêt du mois : ' : ''); ?>
+                        <h3 style="text-align: left;font-family: sans-serif;">{{ $title }}{{ $arret->reference }} du {{ $arret->pub_date->formatLocalized('%d %B %Y') }}</h3>
                         <p class="abstract">{!! $arret->abstract !!}</p>
                         <div>{!! $arret->pub_text !!}</div>
                         <p><a href="{{ asset('files/arrets/'.$arret->file) }}">Télécharger en pdf</a></p>
