8c0ca1e8228c615c3ffc338394cf92f78a8aef36
Cindy Leschaud <cindy.leschaud@gmail.com>
2016-10-03 19:13:42 +0200

    add alert html partial

--- resources/views/backend/layouts/master.blade.php
+++ resources/views/backend/layouts/master.blade.php
@@ -73,6 +73,7 @@
 
                 <!-- messages and errors -->
                 @include('backend.partials.message')
+                @include('alert::alert')
 
                 <!-- Contenu -->
                 @yield('content')
