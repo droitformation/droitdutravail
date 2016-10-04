6f0a5fb242bdf2d3b14940528f4103adac5e83e3
Cindy Leschaud <cindy.leschaud@gmail.com>
2016-10-04 08:36:54 +0200

    change 503 page

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Be right back.</title>
+        <title>Maintenance. De retour dans quelques minutes</title>
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,400italic,600,600italic,700italic,700,800,800italic,300italic,300' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
 
         <style>
             html, body {
@@ -14,10 +15,10 @@
                 margin: 0;
                 padding: 0;
                 width: 100%;
-                color: #B0BEC5;
+                color: #666;
                 display: table;
-                font-weight: 100;
-                font-family: 'Lato';
+                font-weight: 200;
+                font-family: 'Open Sans',Helvetica,Arial,sans-serif;
             }
 
             .container {
@@ -32,15 +33,31 @@
             }
 
             .title {
-                font-size: 72px;
+                font-size: 32px;
                 margin-bottom: 40px;
             }
+
+            h1{
+                font-size: 62px;
+                font-weight: 200;
+                margin-bottom: 10px;
+            }
+            h2{
+                font-size: 45px;
+                font-weight: 600;
+                margin-bottom: 20px;
+            }
+
         </style>
     </head>
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Be right back.</div>
+                <div class="title">
+                    <h1>Droit du travail</h1>
+                    <h2><i class="fa fa-exclamation-triangle"></i> &nbsp;En maintenance</h2>
+                    De retour dans quelques minutes
+                </div>
             </div>
         </div>
     </body>
