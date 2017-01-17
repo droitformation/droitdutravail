f5360aaeb5af81b537b5d662f04136290afd8ea3
Cindy Leschaud <cindy.leschaud@gmail.com>
2017-01-17 08:14:12 +0100

    only last send newsletter in menu

--- resources/views/partials/navigation.blade.php
+++ resources/views/partials/navigation.blade.php
@@ -3,7 +3,7 @@
     <ul class="menu">
         <li><a class="{{ Request::is( '/') ? 'active' : '' }}" href="{{ url('/') }}">Accueil</a></li>
         <li><a class="{{ Request::is( 'jurisprudence') ? 'active' : '' }}" href="{{ url('jurisprudence') }}">Jurisprudence</a></li>
-        <li><a href="{{ url('newsletter/campagne/'.$newsletters->campagnes->sortByDesc('id')->first()->id) }}">Newsletter</a></li>
+        <li><a href="{{ url('newsletter/campagne/'.$newsletters->campagnes->sortByDesc('id')->where('send_at',null)->first()->id) }}">Newsletter</a></li>
         <li><a class="{{ Request::is( 'auteur') ? 'active' : '' }}" href="{{ url('auteur') }}">Auteurs</a></li>
         <li><a class="{{ Request::is( 'colloque') ? 'active' : '' }}" href="{{ url('colloque') }}">Colloques</a></li>
         <li><a class="{{ Request::is( 'contact') ? 'active' : '' }}" href="{{ url('contact') }}">Contact</a></li>
